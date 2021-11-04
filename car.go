4ce200df70f50165ff20982d2d28cac2a0f186ce
Stuart Smith <stuartsayswhat@googlemail.com>
2021-11-04 19:07:17 +0000

    first commit

---
+++ car.go
@@ -0,0 +1,31 @@
+package carpedia
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	qs "github.com/google/go-querystring/query"
+)
+
+type CarService service
+
+type CarOptions struct {
+}
+
+func (c *CarService) Car(ctx context.Context, opts *CarOptions) (*Response, error) {
+
+	params, err := qs.Values(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	u := fmt.Sprintf("home/%s", params.Encode())
+	req, err := c.client.NewRequest(http.MethodGet, u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	return c.client.defaultDo(ctx, req)
+}
---
+++ carpedia.go
@@ -0,0 +1,132 @@
+package carpedia
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+const (
+	defaultBaseURL = "http://localhost:8100/home"
+)
+
+type Response struct {
+	*http.Response
+}
+
+type service struct {
+	client *Client
+}
+
+type ClientOpts struct {
+	ApiKey     string
+	ApiSecret  string
+	BaseURL    *url.URL
+	RetryLimit int
+	RetryDelay time.Duration
+	Timeout    time.Duration
+}
+
+type Client struct {
+	client *http.Client
+	opts   ClientOpts
+
+	Car *CarService
+}
+
+func NewClient(httpClient *http.Client, opts ClientOpts) *Client {
+	baseURL, _ := url.Parse(defaultBaseURL)
+	c := &Client{
+		client: httpClient,
+		opts: ClientOpts{
+			BaseURL: baseURL,
+		},
+	}
+	return c
+	// c.Search = (*SearchService)
+
+}
+
+func (c *Client) Get(ctx context.Context)
+
+type APIError struct {
+	Code    int    `json: "code"`
+	Message string `json: "message"`
+}
+
+func (c *Client) defaultDo(ctx context.Context, req *http.Request) (*Response, error) {
+	/*
+		No oauth yet
+	*/
+
+	var resp *Response
+	var err error
+	for i := 0; ; i++ {
+
+		// resp, err := c.Do(ctx, req)
+		resp, err := c.client.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		if resp.StatusCode != http.StatusTooManyRequests {
+			break
+		}
+		if i >= c.opts.RetryLimit {
+			break
+		}
+		time.Sleep(c.opts.RetryDelay)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var apiErr APIError
+	err = json.Unmarshal(body, &apiErr)
+	if err != nil {
+		return nil, fmt.Errorf("HTTP %s: %s", resp.Status, body)
+	}
+	return resp, nil
+
+}
+
+func (c *Client) NewRequest(method, urlStr string, body interface{}) (*http.Request, error) {
+	if !strings.HasSuffix(c.opts.BaseURL.Path, "/") {
+		return nil, fmt.Errorf("BaseURL must have trailing slash, but %q does not", c.opts.BaseURL)
+	}
+
+	// parse BaseURL
+	u, err := c.opts.BaseURL.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	// encode body into io.ReadWriter
+	var buf io.ReadWriter
+	if body != nil {
+		buf = &bytes.Buffer{}
+		json.NewEncoder(buf).Encode(&body)
+	}
+
+	// create request
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+
+	if body != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+
+	// TODO: userAgent?
+	return req, nil
+
+}
